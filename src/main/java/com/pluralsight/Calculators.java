96842be5e6ae3667d26c0cea4360e3b974b8aab9
jevaughnfowler <jevaughnfowler@gmail.com>
2025-04-21 07:58:09 -0400

    yet to be finish future and present value cal

--- src/main/java/com/pluralsight/Calculators.java
+++ src/main/java/com/pluralsight/Calculators.java
@@ -14,52 +14,80 @@ public class Calculators {
         System.out.println("A: Mortgage \nB: Future Value \nC: Present Value ");
 
 
-        String choice = scanner.nextLine();
+        String choice = scanner.nextLine().toUpperCase();
 
+        //Building the different types of calculators after interaction
 
         if(choice.equals ("A")){
             System.out.println("You Choose Mortgage Calculator!");
+
+
+            // A: Mortgage
+            System.out.println("Please fill out the following " +
+                    "\nPrincipal " +
+                    "\nAnnual Interest Rate(APR) " +
+                    "\nLoan Years \n" );
+
+            System.out.println("Enter The Following \n");
+
+            System.out.println("Principal-What is the amount? eg.(300,000)");
+            int principal = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Annual Interest Rate(APR) eg.(3.20)");
+            float apr = scanner.nextFloat();
+            scanner.nextLine();
+
+            System.out.println("Loan Years-How many years is the loan For eg.(15 years etc)");
+            int yearsOfLoan = scanner.nextInt();
+            scanner.nextLine();
+
+            //Calculate output for the calculators
+
+
+            double monthlyRate = apr / 100 / 12;
+            int totalPayments = yearsOfLoan * 12;
+
+            double monthlyPayment = principal * (monthlyRate * Math.pow(1 + monthlyRate, totalPayments)) /
+                    (Math.pow(1 + monthlyRate, totalPayments) - 1);
+
+            System.out.println("Monthly Payment is: $" + String.format("%.2f", monthlyPayment));
+
         }else if (choice.equals ("B")){
             System.out.println("You Choose Future Value Calculator!");
+            //Future Value
+            System.out.println("Please Enter the Following Below: \n");
+            System.out.println("Principal (eg. Amount deposit)");
+            int principal = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("Annual Interest Rate(APR)");
+            float apr = scanner.nextFloat();
+            scanner.nextLine();
+
+            System.out.println("Number Of Years ");
+            int numberOfYears = scanner.nextInt();
+            scanner.nextLine();
+
+
+
+
+
+
+
         }else if (choice.equals ("C")){
             System.out.println("You Choose Present Value Calculator!");
+
         }else{
             System.out.println("Invalid input please try again");
             scanner.next();
         }
 
-        //Building the different types of calculators after interaction
-        // A: Mortgage
-        System.out.println("Please fill out the following " +
-                "\nMonthly Payment " +
-                "\nPrincipal " +
-                "\nAnnual Interest Rate(APR) " +
-                "\nLoan Years \n" );
-
-        System.out.println("Enter The Following \n");
-
-        System.out.println("Principal-What is the amount? eg.(300,000)");
-        int principal = scanner.nextInt();
-        scanner.nextLine();
-
-        System.out.println("Annual Interest Rate(APR) eg.(3.20)");
-        float apr = scanner.nextFloat();
-        scanner.nextLine();
-
-        System.out.println("Loan Years-How many years is the loan For eg.(15 years etc)");
-        int yearsOfLoan = scanner.nextInt();
-        scanner.nextLine();
-
-        //Calculate output for mortgage
 
 
-        double monthlyRate = apr / 100 / 12;
-        int totalPayments = yearsOfLoan * 12;
 
-        double monthlyPayment = principal * (monthlyRate * Math.pow(1 + monthlyRate, totalPayments)) /
-                (Math.pow(1 + monthlyRate, totalPayments) - 1);
 
-        System.out.println("Monthly Payment is: $" + String.format("%.2f", monthlyPayment));
+
 
 
 
